377dc02d1a1a2a7f3ccac9ce57d6d89e99162354
Kamanaka5502 <samgreenrevita@gmail.com>
2025-11-23 20:37:09 -0700

    Create main.ts

---
+++ server/server/src/server/src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+  });
+
+  await app.listen(3000);
+  console.log('🚀 Server running at http://localhost:3000');
+}
+
+bootstrap();
